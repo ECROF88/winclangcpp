09474cc08d971678aed8b70c8957c5708d00c195
ECROF88 <maomao8672@outlook.com>
2024-08-04 16:30:44 +0800

    1

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 set -e
 #clear test.exe
+#进入根目录
+export CXX=clang++
 rm -rf build
 cmake . -B build -G Ninja 
 Ninja -C build
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 // main.cpp
 #include <iostream>
 #include <print>
+#include <string>
+#include <vector>
 int pp(int a) { return a; }
 int main()
 {
   std::cout << "3 + 1 = " << std::endl;
-  // std::print("aa\n");
-  std::cout << __cplusplus << std::endl;
-  int adadadaaaaaadadadadadadadadadadadadaddad = 1;
+  std::cout << "C++ Standard: " << __cplusplus << std::endl;
+
+// 判断并输出编译器信息
+#ifdef __clang__
+  std::cout << "Compiler: Clang" << std::endl;
+  std::cout << "Clang Version: " << __clang_major__ << "." << __clang_minor__ << "."
+            << __clang_patchlevel__ << std::endl;
+#elif defined(__GNUC__)
+  std::cout << "Compiler: GCC" << std::endl;
+  std::cout << "GCC Version: " << __GNUC__ << "." << __GNUC_MINOR__ << "."
+            << __GNUC_PATCHLEVEL__ << std::endl;
+#elif defined(_MSC_VER)
+  std::cout << "Compiler: MSVC" << std::endl;
+  std::cout << "MSVC Version: " << _MSC_VER << std::endl;
+#else
+  std::cout << "Unknown Compiler" << std::endl;
+#endif
   return 0;
 }
