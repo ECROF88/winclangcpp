a39ec277ffc2e0226ae1faacfcb2b7640275f8ca
ECROF88 <maomao8672@outlook.com>
2024-08-17 00:49:18 +0800

    Update main.cpp

--- src/co_/main.cpp
+++ src/co_/main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <coroutine>
+#include <iostream>
 
 #include "debug.hpp"
 struct SuspendUntilDone {
@@ -44,24 +45,40 @@ struct RepeatAwaitable  // awaitable(operator->)
     调用co_await时，会调用operator co_await()，返回一个awaiter
   */
 };
+struct PreviousAwaiter {
+  std::coroutine_handle<> mPrevious;
+  bool await_ready() const noexcept { return false; }
+  std::coroutine_handle<> await_suspend(std::coroutine_handle<> coroutine) const noexcept
+  {
+    if (mPrevious)
+      return mPrevious;
+    else
+      return std::noop_coroutine();  // 如果没有上一个协程，那么直接返回noop_coroutine
+  }
+  void await_resume() const noexcept {}
+};
 
 struct Promise {
   auto initial_suspend() { return std::suspend_always(); }
 
-  auto final_suspend() noexcept { return std::suspend_always(); }
+  auto final_suspend() noexcept  // 这个的作用 在协程结束时调用
+  {
+    // return std::suspend_always();
+    return PreviousAwaiter(mPrevious);  // 如果没有上一个协程，那么直接返回noop_coroutine
+  }
 
   void unhandled_exception() { throw; }
 
   auto yield_value(int ret)
   {
     mRetValue = ret;
-    return RepeatAwaiter();
-    // return SuspendUntilDone();
-    // return std::suspend_always();
+    // return RepeatAwaiter();
+    // return SuspendUntilDone();  // 直到done才挂起
+    return std::suspend_always();
   }
 
-  //   void return_value(int ret) { mRetValue = ret; }
-  void return_void() { mRetValue = 0; }
+  void return_value(int ret) { mRetValue = ret; }
+  //   void return_void() { mRetValue = 0; }
 
   //   auto yield_value(int ret)
   //   {
@@ -74,9 +91,16 @@ struct Promise {
   }
 
   int mRetValue;
+  std::coroutine_handle<> mPrevious = nullptr;
 };
 
 struct Task {
+  Task(Task&&) = delete;
+  ~Task()
+  {
+    debug(), "Task析构";
+    mCoroutine.destroy();
+  }
   using promise_type = Promise;
 
   Task(std::coroutine_handle<promise_type> coroutine) : mCoroutine(coroutine) {}
@@ -86,42 +110,66 @@ struct Task {
 struct WorldTask {
   using promise_type = Promise;
   WorldTask(std::coroutine_handle<promise_type> coroutine) : mCoroutine(coroutine) {}
-  std::coroutine_handle<promise_type> mCoroutine;
-  bool await_ready() const noexcept { return false; }
-  std::coroutine_handle<> await_suspend(std::coroutine_handle<> coroutine) const noexcept
+  WorldTask(WorldTask&&) = delete;
+  ~WorldTask()
   {
-    // if (coroutine.done())
-    //   return std::noop_coroutine();
-    // else
-    return mCoroutine;
+    debug(), "worldTask析构";
+    mCoroutine.destroy();
   }
-  void await_resume() const noexcept {}
+
+  struct Awaiter {
+    bool await_ready() const noexcept { return false; }
+    std::coroutine_handle<> await_suspend(
+        std::coroutine_handle<> coroutine) const noexcept
+    {
+      // 当协程遇到 co_await 表达式时会调用这个方法。
+      // 传入的 coroutine 参数是当前正在执行的协程的句柄。
+      // if (mCoroutine.promise().mPrevious == nullptr)
+      mCoroutine.promise().mPrevious = coroutine;
+      // 不加这句话： main得到hello结果为-445748895
+      debug(), "world await_suspend";
+      return mCoroutine;
+    }
+    // void await_resume() const noexcept {}
+    auto await_resume() const noexcept
+    {
+      return mCoroutine.promise().mRetValue;
+    }  // co_await的返回值
+    std::coroutine_handle<promise_type> mCoroutine;
+  };
+
+  auto operator co_await() { return Awaiter{mCoroutine}; }
+
+  std::coroutine_handle<promise_type> mCoroutine;
 };
 
 WorldTask world()
 {
   debug(), "world";
-
-  co_return;
+  co_yield 99;
+  co_return 1234;
 }
 Task hello()
 {
-  debug(), "开始构建worldtask";
-  WorldTask worldTask = world();
-  debug(), "完成构建worldtask";
-  co_await world();
+  WorldTask w = world();
+  int i = co_await w;
+  debug(), "task 获取了world的返回值：", i;
+  debug(), "world 返回：", w.mCoroutine.promise().mRetValue;
+  i = co_await w;
+  debug(), "task 获取了world的返回值：", i;
+  //   debug(), "world 返回：",
+  //   w.mCoroutine.promise().mRetValue;//把这个加在await_resume里面上面就能用i获取返回值
   debug(), "world finish";
   debug(), "hello 42";
   co_yield 42;
   debug(), "hello 22";
   co_yield 12;
-  debug(), "hello 6";
+  //   co_await w;
+  debug(), "hello 12";
   co_yield 6;
   debug(), "hello 结束";
-  //   co_return 22;
-  //   co_return 44;
-  //   co_return 44;
-  co_return;
+  i++;
+  co_return i;
 }
 
 int main()
@@ -129,7 +177,6 @@ int main()
   debug(), "main即将调用hello";
   Task t = hello();
   debug(), "main调用完了hello";
-  t.mCoroutine.resume();
   while (!t.mCoroutine.done()) {
     t.mCoroutine.resume();
     debug(), "main得到hello结果为", t.mCoroutine.promise().mRetValue;
